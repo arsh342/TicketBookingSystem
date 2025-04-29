e7b26ce29a059c9e3c7b3cd3b25dce27196a08c2
arsh342 <arshth134@gmail.com>
2025-04-29 09:10:32 +0530

    seating arrangement

--- src/TicketBookingSystem/SeatMap.java
+++ src/TicketBookingSystem/SeatMap.java
@@ -13,6 +13,7 @@ public class SeatMap {
         }
         System.out.println();
 
+
         for (int i = 0; i < seats.length; i++) {
             System.out.printf("Row %2d: ", i + 1);
             for (int j = 0; j < seats[i].length; j++) {
